94fc3860988ab140c6a0252a264d2d150f617074
Jackson-Harrison <89489513+Jackson-Harrison@users.noreply.github.com>
2022-03-30 21:28:04 -0700

    CWC Prototype 1 Complete
    Did not commit on Monday and Tuesday because I was home sick and did not make any progress.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Variables
+    public GameObject player;
+    private Vector3 CameraOffest = new Vector3(0, 5, -7); 
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Camera followes player by adding vector to player position
+        transform.position = player.transform.position + CameraOffest;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Private Variables
+    private float speed = 20.0f;
+    private float turnSpeed = 100.0f;
+
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Getting player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on the horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
